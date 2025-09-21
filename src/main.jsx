e8aec9c9b1870b067a271e28b4b34df1785a10dc
Mohd Kaif <124128116+btwitskaif69@users.noreply.github.com>
2025-09-22 02:00:37 +0530

    fixed the error

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import "./Font.css";
+import "./font.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
